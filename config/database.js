bce13ef3e440381d8f5d3125ae27a21f33a01c3b
Batzolboo <batzol_b@yahoo.com>
2017-09-23 22:06:12 +0800

    after14

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    url: 'mongodb://localhost:27017/mean-angular-2',
+    //url: 'mongodb://localhost:27017/mean-angular-2',
+    uri: 'mongodb://batzol_angular:Ba123@ds147864.mlab.com:47864/angular-2-app-batzol',
     secret: crypto,
-    db: 'mean-angular-2'
+    db: 'angular-2-app-batzol'  //'mean-angular-2'
 }
